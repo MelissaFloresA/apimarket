b57e054f14a2533f566225162f2b0366afa3e1f7
melissa.flores <melissa.abi.flores@gmail.com>
2025-09-28 20:37:22 -0600

    app y config para railway

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-import express from 'express'
-import marketRoutes from './routes/market.routes.js'
+import express from "express";
+import marketRoutes from "./routes/market.routes.js";
 
-const app=express()
+const app = express();
 
 // Middleware para procesar JSON
 app.use(express.json());
@@ -9,12 +9,16 @@ app.use(express.json());
 // Middleware para procesar datos de formularios (opcional, si usas x-www-form-urlencoded)
 app.use(express.urlencoded({ extended: true }));
 
-app.use(marketRoutes)
+app.use(marketRoutes);
 
-app.use((req,res,next)=>{
-    res.status(404).json({
-        message:'Endpoint No encontrado'
-    })
-})
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: "Favor realizar pruebas en los siguientes endpoints:",
+    endpoints: [
+      "https://URL GENERADA Railway /usuarios",
+      "https:// URL GENERADA Railway /productos",
+    ],
+  });
+});
 
 export default app;
